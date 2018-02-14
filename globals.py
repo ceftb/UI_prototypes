9069a1a7e4769542fc48ae387667bc95575ec506
gen <gen@guest-101.isi.edu>
2018-02-14 15:51:06 -0800

    Globals for GUI app

---
+++ globals.py
@@ -0,0 +1,16 @@
+app = None
+acn=0
+tcn=0
+
+actors=dict()
+behaviors=dict()
+constraints=dict()
+events=dict()
+actions=dict()
+methods=dict()
+
+sbuttons=dict()
+slabels=dict()
+
+bstate = "start" # state of current behavior start, (waitd, wait) or (whene, when) or actor, actor, action, method, emit, done
+
