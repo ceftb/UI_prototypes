0c125eea747acf334326ed5b2bca05a6d154a61a
gen <gen@guest-115.isi.edu>
2018-02-26 13:54:32 -0800

    Minor fixes (like order of keyword 'start'/'restart' in action) + made a class object.

--- HighLevelBehaviorLanguage/hlb_parser.py
+++ HighLevelBehaviorLanguage/hlb_parser.py
@@ -1,35 +1,48 @@
-from pyparsing import ParseException, NotAny, Group, Word, Keyword, Literal, alphas, alphanums, nums, White, ZeroOrMore, OneOrMore, Optional
+from pyparsing import ParseException, NotAny, Group, Word, CaselessKeyword, Literal, alphas, alphanums, nums, White, ZeroOrMore, OneOrMore, Optional
 
-when_keyword = (Keyword("when") | Keyword("When") | Keyword("WHEN")).suppress()
-wait_keyword = (Keyword("wait") | Keyword("Wait") | Keyword("WAIT")).suppress()
-emit_keyword = (Keyword("emit") | Keyword("Emit") | Keyword("EMIT")).suppress()
-not_akeyword = (NotAny(emit_keyword) + NotAny(wait_keyword) + NotAny(when_keyword))
-comma = Literal(",").suppress()
-newline = Literal("\n")
-wsp = White().suppress()
-actor = (not_akeyword + Word(alphanums))
-actors = actor + ZeroOrMore((comma + wsp + actor | comma + actor))
-action = (not_akeyword + Word(alphanums) + Optional(wsp + "start" |wsp + "stop" | wsp + "check"))
-event =(not_akeyword + Word(alphanums))
-events = event + ZeroOrMore((comma + wsp + event | comma + event))
-trigger = (when_keyword + events("t_events") | when_keyword + events("t_events") + wait_keyword + nums | wait_keyword + nums)
-hlb_statement = (trigger("trigger") + wsp +  actors("actors") + wsp + action("action") + emit_keyword + events("emit_events") | trigger("trigger") + wsp + actors("actors") + wsp + action("action"))
-#hlb = hlb_statement + newline + hlb
+class HLBParser():
+    when_keyword = CaselessKeyword("when").suppress()
+    wait_keyword = CaselessKeyword("wait").suppress() 
+    emit_keyword = CaselessKeyword("emit").suppress()
+    not_akeyword = (NotAny(emit_keyword) + NotAny(wait_keyword) + NotAny(when_keyword))
+    comma = Literal(",").suppress()
+    newline = Literal("\n")
+    wsp = White().suppress()
+    actor = (not_akeyword + Word(alphanums))
+    actors = actor + ZeroOrMore((comma + wsp + actor | comma + actor))
+    action = (Optional(CaselessKeyword("start") | CaselessKeyword("stop") | CaselessKeyword("restart") | CaselessKeyword("check")) + Word(alphanums))	
+    event =(not_akeyword + Word(alphanums))
+    events = event + ZeroOrMore((comma + wsp + event | comma + event))
+    trigger = (when_keyword + events("t_events") | when_keyword + events("t_events") + wait_keyword + nums | wait_keyword + nums)
+    hlb_statement = (trigger("trigger") + wsp +  actors("actors") + wsp + action("action") + emit_keyword + events("emit_events") | trigger("trigger") + wsp + actors("actors") + wsp + action("action"))
+    #hlb = hlb_statement + newline + hlb
+    
+    
+    def parse_stmt(self, statement):
+        """Parses an HLB statement and returns a tuple: (triggers, actors, action, emit_events)
+        """
+        try:
+            parsed = self.hlb_statement.parseString(statement)
+        except ParseException as pe:
+            print("WARNING: Could not parse HLB statement:\n\t%s" % (statement))
+            return(None, None, None, None)
 
-while True:
-    print("Enter statement.")
-    line = input()
-    try:
-        parsed = hlb_statement.parseString(line)
-        print(parsed)
-        print("Trigger(s): %s" % parsed.t_events)
-        print("Actors: %s" %  ','.join(parsed.actors))        
-        print("Action: %s" %  parsed.action)        
-        print("Emit events: %s" % parsed.emit_events)
-    except ParseException as pe:
-        print(pe)
-        continue
+        return(parsed.t_events, parsed.actors, parsed.action, parsed.emit_events)
+
+def testParser():
+    parser = HLBParser()
+    while True:
+        print("Enter statement.")
+        line = input()
+        (t_events, actors, action, e_events) = parser.parse_stmt(line)
+        if actors != None:
+            print("Trigger(s): %s" % t_events)
+            print("Actors: %s" %  actors)        
+            print("Action: %s" %  action)        
+            print("Emit events: %s" % e_events)
     
 
+if __name__ == "__main__":
+    testParser()
     
     
